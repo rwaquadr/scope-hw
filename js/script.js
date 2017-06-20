f138ac440c7e7fb38d040aaaa8ca6d9ef1c5d7e1
Robb Aquadro <rwaquadr@gmail.com>
2017-06-19 20:35:50 -0500

    added if statement for end of show, and updated showMySlide function

--- js/script.js
+++ js/script.js
@@ -3,28 +3,29 @@ slideshow = {
 	currentPhotoIndex: 1,
 	nextPhoto : function(){
 		return getNext (this.currentPhotoIndex);
+		
 	},
 	prevPhoto : function(){
 		return getPrev(this.currentPhotoIndex);
 	},
 }
 		
+function getCurrent(i){return i;}
 function getNext(i){return i +1;}
 function getPrev(i){return i -1;}
+
 function showMySlide(){
-	console.log('now ' + slideshow.photoList.currentPhotoIndex());
+	console.log('now ' + slideshow.currentPhotoIndex);
 	console.log('next ' + slideshow.nextPhoto());
 	console.log('prev ' + slideshow.prevPhoto());
-}
+
+	if(slideshow.currentPhotoIndex > 2 || slideshow.currentPhotoIndex < 1){
+		console.log('End of slideshow');
+	};
+};
 
 
-console.log(slideshow.currentPhotoIndex);
-console.log(slideshow.nextPhoto());
-console.log(slideshow.prevPhoto());
-
-
-
-
+console.log(showMySlide());
 
 
 
